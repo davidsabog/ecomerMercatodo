c8346a2fa2443a2aa742c1b14cbfcd87b0b9f1d0
David Sabogal <davidsabog@outlook.com>
2020-07-25 19:14:54 -0500

    Finalizado prototipado de las vitas princilapes(vista raiz, login, register, se agrega el metodo SoftDelete, Se define la estructura de layouts( main_layout, main_partials), y se crea la navegacion basica entre estas vistas

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//Activate `verify' route for email autentication
+Auth::routes(['verify'=>true]);
+
+// ->middleware for acces only if user has been email verified
+Route::get('/home', 'HomeController@index')->name('home')->Middleware('verified');
